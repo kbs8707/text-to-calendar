fb24fb88c6c620192e07bcc58d0a9d125e395584
John <kbs8707@hotmail.com>
2020-01-09 19:15:41 -0500

    init

---
+++ script.js
@@ -0,0 +1,18 @@
+function textToData() {
+
+}
+
+function dataToFile() {
+    
+}
+
+function buildEvent() {
+    "BEGIN:VCALENDAR"
+    "BEGIN:VEVENT"
+    "END:VEVENT"
+	"END:VCALENDAR"
+}
+
+function download() {
+    
+}
